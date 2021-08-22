581219ddb0280ea5b86996b17e4f739aca487f5b
Andrew Falkowski <>
2021-08-22 14:47:02 -0600

    copied bootstrap materialism files

--- scripts/rss_loader.js
+++ scripts/rss_loader.js
@@ -8,16 +8,28 @@ fetch(RSS_URL)
     const items = data.querySelectorAll("item");
     let html = ``;
     items.forEach(el => {
-      html += `
-        <li>
-  				<div class='episode'>
-  				<a href="${el.querySelector("enclosure").getAttribute('url')}" target="_blank" rel="noopener noreferrer">${el.querySelector("title").innerHTML}</a>
-            <div class='description'>
-              <p>${el.querySelector("description").innerHTML}</p>
+        const { textContent: dscr } = el.querySelector('description');
+        const split_dscr = dscr.split('</p>');
+        const first_p = split_dscr[0];
+        // const start = `Keywords: `;
+        // const end = `</p>`;
+        // const keywords = dscr.split(start)[1].split(end)[0];
+        const { textContent: date } = el.querySelector('pubDate');
+        const short_date = date.split(' ').slice(0,4).join(' ')
+        html += `
+
+            <div class="episode card border-white mb-3 bg-transparent rounded-0">
+              <div class="card-body text-white">
+                <div class="card-header bg-transparent border-white mb-3 px-0">
+                        <h5 class="text-white card-title"><span class="text-red font-monospace">${el.querySelector("enclosure").nextElementSibling.innerHTML}:</span> ${el.querySelector('description').nextElementSibling.innerHTML}</h5>
+                        <p class="card-text text-white font-monospace"><small><mark style="background-color:#3B658F; color:white">[${el.querySelector('pubDate').nextElementSibling.innerHTML}]</mark></small></p>
+                </div>
+                <p>${first_p}</p>
+                <a href="${el.querySelector("enclosure").getAttribute('url')}" class="card-link text-orange font-monospace">Listen Now!</a>
+              </div>
             </div>
-          </div>
-        </li>
-      `;
+
+        `;
     });
     document.getElementById("episode_list").insertAdjacentHTML("beforeend", html);
   });
